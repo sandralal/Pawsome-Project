939b666ba4528ce9581528888ca9e63556666cb9
Sandra Lal <sandra.20ubc150@mariancollege.org>
2022-10-06 09:26:39 +0530

    update

--- home.php
+++ home.php
@@ -22,13 +22,13 @@ href="style-home.css">
                     <img src="logo.jpg" style="float: left">
                     <span class="heading" style="float: left;">PaweSome</span>
                 </div>
-         <div>
+            <div >
                 <ul>
                 
                     <li ><a href="login.php" >Login</a></li>
                     <li><a href="#contact">Services</a></li>
                     <li><a href="#about">About</a></li>
-                    <li  ><a  href="index.html">Home</a></li>
+                    <li  ><a  href="home.php">Home</a></li>
                 
                 </ul>
         </div>
